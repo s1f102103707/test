b3f24fa8cab2d5fdde4d0d31aa38b8ca3f54362c
s1f102103707 <85672513+s1f102103707@users.noreply.github.com>
2023-08-23 15:22:59 +0900

    整理

--- helloworld/src/index.ts
+++ helloworld/src/index.ts
@@ -2,37 +2,24 @@ import axios from 'axios';
 import dotenv from 'dotenv';
 import 'dotenv/config';
 import { OpenAI } from 'langchain/llms';
-
-// .env ファイルから環境変数を読み込む
 dotenv.config();
-
-// LLMの準備
 const llm = new OpenAI({ temperature: 0.9 });
-
-// 指定のURLから情報を読み込んで指定の行動を実行する関数
 const processURL = async (url: string) => {
   try {
-    // URLからデータを取得
     const response = await axios.get(url);
+    console.log('URLから取得したデータ:', response.data);
 
-    // データを取得したら、ここで適切な行動を実行する
-    //console.log('URLから取得したデータ:', response.data);
-
-    // LLMにデータを入力として渡して応答を取得
-    const llmResponse = await generateLLMResponse(response.data); // LLMモデルによる文生成
+    const llmResponse = await generateLLMResponse(response.data);
     console.log('LLMの応答:', llmResponse);
   } catch (error: any) {
     console.error('エラー:', error.message);
   }
 };
 
-// LLMモデルによる文生成
 const generateLLMResponse = async (data: any) => {
   try {
-    // 買い時と売り時の情報を生成するための文生成リクエスト
     const prompt = `データ:${data}\nあなたが買い時だと思う日時をdataからいくつか挙げてください。\nあなたが売り時だと思う日時をdataからいくつか挙げてください。`;
 
-    // LLMに文生成リクエストを送信
     const llmResponse = await llm.call(prompt);
     return llmResponse;
   } catch (error) {

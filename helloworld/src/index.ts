255b2905a31e56cc6f89dc50928f3cd54486cd0e
s1f102103707 <85672513+s1f102103707@users.noreply.github.com>
2023-08-24 19:33:14 +0900

    langchain

--- helloworld/src/index.ts
+++ helloworld/src/index.ts
@@ -1,18 +1,20 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
-import 'dotenv/config';
-import { OpenAI } from 'langchain/llms';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import {
+  ChatPromptTemplate,
+  HumanMessagePromptTemplate,
+  SystemMessagePromptTemplate,
+} from 'langchain/prompts';
 
-dotenv.config();
-
-const llm = new OpenAI({ temperature: 0.9 });
+dotenv.config(); // dotenvの初期化はここで行う
 
 const alphaVantageKey = process.env.Alpha_Vantage_Key;
 
 const Get5minData = async () => {
   try {
-    const symbol = 'JPYUSD'; // 日本円とアメリカドル
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=5min&apikey=${alphaVantageKey}`;
+    const symbol = 'JPYUSD';
+    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&apikey=${alphaVantageKey}`;
     const response = await axios.get(url);
     return response.data;
   } catch (error) {
@@ -21,51 +23,22 @@ const Get5minData = async () => {
   }
 };
 
-const Judge5minData = async () => {
-  try {
-    const minuteData = await Get5minData();
-    console.log('５分毎のデータ:', minuteData);
+const runTemplate = async () => {
+  const minuteData = await Get5minData();
+  const timestamps = Object.keys(minuteData['Time Series (1min)']);
+  const chatPrompt = ChatPromptTemplate.fromPromptMessages([
+    SystemMessagePromptTemplate.fromTemplate(`あなたは{role}です。`),
+    HumanMessagePromptTemplate.fromTemplate(
+      '{input}をもとにあなたが買い時だと思う日時をデータから一つ挙げてください。'
+    ),
+  ]);
 
-    const timestamps = Object.keys(minuteData['Time Series (5min)']);
-    const prompt = `データ:${timestamps.join(
-      ', '
-    )}\nあなたが買い時だと思う日時をデータからどのデータを評価して選んだかを明確にして理由と共に一つ挙げてください。\nあなたが売り時だと思う日時をデータからどのデータを評価して選んだかを明確にして理由と共に一つ挙げてください。`;
+  const chat = new ChatOpenAI({ temperature: 0 });
+  const prompt = await chatPrompt.formatPromptValue({ role: '堅実な性格', input: timestamps });
+  const messages = prompt.toChatMessages();
 
-    const llmResponse = await llm.call(prompt);
-    console.log('LLMの応答:', llmResponse);
-  } catch (error: any) {
-    console.error('エラー:', error.message);
-  }
+  const res = await chat.call(messages);
+  console.log(res);
 };
 
-const GetDailyData = async () => {
-  try {
-    const symbol = 'JPYUSD'; //日本円とアメリカドル
-    const url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${symbol}&apikey=${alphaVantageKey}`;
-    const response = await axios.get(url);
-    return response.data;
-  } catch (error) {
-    console.error('Alpha Vantageエラー:', error);
-    throw new Error('Alpha Vantageからデータを取得できませんでした。');
-  }
-};
-
-const JudgeDailyData = async () => {
-  try {
-    const dailyData = await GetDailyData();
-    console.log('1日毎のデータ:', dailyData);
-
-    const timestamps = Object.keys(dailyData['Time Series (Daily)']);
-    const prompt = `データ:${timestamps.join(
-      ', '
-    )}\nあなたが買い時だと思う日時をデータからどのデータを評価して選んだかを明確にして理由と共に一つ挙げてください。\nあなたが売り時だと思う日時をデータからどのデータを評価して選んだかを明確にして理由と共に一つ挙げてください。`;
-
-    const llmResponse = await llm.call(prompt);
-    console.log('LLMの応答:', llmResponse);
-  } catch (error: any) {
-    console.error('エラー:', error.message);
-  }
-};
-
-Judge5minData();
-JudgeDailyData();
+runTemplate();
